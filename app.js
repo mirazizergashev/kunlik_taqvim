48fb75785e0846d8fd91c7b2c4999ba55f1eac46
mirazizergashev <mirazizmirmuxsinovich@gmail.com>
2021-04-14 16:54:03 +0500

    kunlik taqvim

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Jimp = require("jimp");
 
-var fileName = 'test.jpg';
-var imageCaption = 'Bu yerda ismingiz turadi.';
+var fileName = '2_1.jpg';
+// var imageCaption = 'Bu yerda ismingiz turadi.';
 var loadedImage;
 
 Jimp.read(fileName)
     .then(function (image) {
         loadedImage = image;
-        return Jimp.loadFont(Jimp.FONT_SANS_128_WHITE);
+        return Jimp.loadFont(Jimp.FONT_SANS_128_BLACK);
     })
     .then(function (font) {
-        loadedImage.print(font, 450, 200, imageCaption)
+        loadedImage.print(font, 670, 1445, "Jonibek")
         .write(`new.png`);
         // .write(`${fileName}.png`);
     })
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const _ = require('lodash');
@@ -7,12 +8,19 @@ const app = express();
 
 const vaqt = `${+new Date()}`;
 var today = new Date();
+const todayq = new Date().toLocaleString('uz-US', {
+  timeZone: 'Asia/Tashkent'
+});
+
+// console.log(nDate)
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
-today = dd + '.' + mm + '.' + yyyy;
-
+// today = dd + '.' + mm + '.' + yyyy;
+today=(todayq.substr(0,10).replace('/','.').replace('/','.'))
+// console.log(today)
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.get('', (req, res) => {
@@ -25,6 +33,32 @@ app.get("/:id",(req,res)=>{
     // res.download(__dirname+ `/${req.params.id}/${today}.png`);
     res.sendFile(__dirname+ `/public/${req.params.id}/${today}.png`)
   })
+
+app.get("/tabrik", (req, res) => {
+  var fileName = '2_1.jpg';
+  // var imageCaption = 'Bu yerda ismingiz turadi.';
+  var loadedImage;
+
+  Jimp.read(fileName)
+    .then(function (image) {
+      loadedImage = image;
+      console.log(1)
+      return Jimp.loadFont(Jimp.FONT_SANS_128_BLACK).then(function (font) {
+        loadedImage.print(font, 670, 1445, `${req.query.ism}`)
+          .write(path.join(__dirname, `public/img/${req.query.ism}.png`));
+        console.log(2)
+        // .write(`${fileName}.png`);
+      }).then(() => {
+        res.redirect(`/img/${req.query.ism}.png`);
+
+      });
+    })
+
+    .catch(function (err) {
+      console.error(err);
+    });
+
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, (req, res) => {
